8480b5f8db12b5e1afae6369f4b9376b1cf66e82
Jefferson C <jcherton@hotmail.com>
2023-09-25 00:48:56 -0500

    .Adding necessary files for Azure Web App

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+app = express()
+
+var url = require('url');
+
+const port = process.env.PORT || 3000
+const majorVersion = 1
+const minorVersion = 2
+
+// Use Express to publish static HTML, CSS, and JavaScript files that run in the browser. 
+app.use(express.static(__dirname + '/static'))
+
+// Custom 404 page.
+app.use((request, response) => {
+  response.type('text/plain')
+  response.status(404)
+  response.send('404 - Not Found')
+})
+
+// Custom 500 page.
+app.use((err, request, response, next) => {
+  console.error(err.message)
+  response.type('text/plain')
+  response.status(500)
+  response.send('500 - Server Error')
+})
+
+app.listen(port, () => console.log(
+  `Express started at \"http://localhost:${port}\"\n` +
+  `press Ctrl-C to terminate.`)
+)
