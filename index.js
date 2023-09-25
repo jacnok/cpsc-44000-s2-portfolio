a82d1dc55c5517f418eb86cd9d567d4d0708cdd9
jacnok <44982807+jacnok@users.noreply.github.com>
2023-09-25 01:59:58 -0500

    Update index.js
    Let's play with this.

--- index.js
+++ index.js
@@ -1,31 +1,14 @@
-const express = require('express')
-app = express()
+const express = require("express");
+const app = express();
 
-var url = require('url');
+app.use(express.static(__dirname + '/client'))
 
 const port = process.env.PORT || 3000
-const majorVersion = 1
-const minorVersion = 2
-
-// Use Express to publish static HTML, CSS, and JavaScript files that run in the browser. 
-app.use(express.static(__dirname + '/static'))
-
-// // Custom 404 page.
-// app.use((request, response) => {
-//   response.type('text/plain')
-//   response.status(404)
-//   response.send('404 - Not Found')
-// })
-
-// Custom 500 page.
-app.use((err, request, response, next) => {
-  console.error(err.message)
-  response.type('text/plain')
-  response.status(500)
-  response.send('500 - Server Error')
+app.get('/test', function(request, response) {
+	response.type('text/plain')
+	response.send('Node.js and Express running on port='+port)
 })
 
-app.listen(port, () => console.log(
-  `Express started at \"http://localhost:${port}\"\n` +
-  `press Ctrl-C to terminate.`)
-)
+app.listen(port, function() {
+	console.log("Server is running at http://localhost:3000/")
+})
